3469ad24306742807b5737f8707e71b33ff0e2d0
David Collien <david@openlearning.com>
2019-07-01 16:04:30 +1000

    add tab-layout boilerplate

---
+++ tab-layout/build-example/precache-manifest.c6bad417e64368b59cb9529c8315e931.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8f0e5df3b036fbebdd0349d4620041e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc842742d4ed550c56cb",
+    "url": "/static/css/main.0cf15e93.chunk.css"
+  },
+  {
+    "revision": "6438655bfbf0a93fadea",
+    "url": "/static/js/2.8ee3c21b.chunk.js"
+  },
+  {
+    "revision": "dc842742d4ed550c56cb",
+    "url": "/static/js/main.a01c2b91.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
